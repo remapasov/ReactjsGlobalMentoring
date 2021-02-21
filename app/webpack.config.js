ef089ad8b76770b7e2dbd4ff6d5221af524f48ff
Rem Apasov <remapasov@yandex.ru>
2021-02-21 19:20:27 +0300

    Configure Webpack, configure DEV and PROD variations.

---
+++ app/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const mode = process.env.NODE_ENV === "prod" ? "production" : "development";
+
+const commonConfig = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
+
+const devConfig = {
+  mode: 'development',
+  devtool: 'inline-source-map'
+};
+
+module.exports = mode === "production" ? commonConfig : merge(commonConfig, devConfig);
