7e3b9a13cf8b50c9d517d6249b9e6d21086f381f
Rem Apasov <remapasov@yandex.ru>
2021-02-22 12:25:41 +0300

    Update webpack config, add jest config.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -12,7 +12,7 @@ const commonConfig = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
       },
